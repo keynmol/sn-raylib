ce0b723559fb2c87c9635b0a439ad76c0c7ee074
Anton Sviridov <keynmol@gmail.com>
2022-03-06 15:22:30 +0000

    Add Raylib example

--- build.sbt
+++ build.sbt
@@ -1,3 +1,4 @@
+Global / onChangedBuildSource := ReloadOnSourceChanges
 scalaVersion := "3.1.1"
 enablePlugins(ScalaNativePlugin, BindgenPlugin)
 
@@ -5,8 +6,25 @@ import bindgen.interface.Binding
 
 bindgenBindings := Seq(
   Binding(
-    baseDirectory.value / "src" / "main" / "resources" / "scala-native" / "header.h",
-    "lib_check",
-    cImports = List("header.h")
+    baseDirectory.value / "raylib" / "src" / "raylib.h",
+    "libraylib",
+    cImports = List("raylib.h")
   )
 )
+
+nativeConfig := {
+  val conf = nativeConfig.value
+  val staticLib = baseDirectory.value / "raylib" / "src" / "libraylib.a"
+  val include = baseDirectory.value / "raylib" / "src"
+
+  val extras =
+    "-framework CoreVideo -framework IOKit -framework Cocoa -framework GLUT -framework OpenGL"
+      .split(" ")
+      .toList
+
+  conf
+    .withLinkingOptions(
+      conf.linkingOptions ++ List(staticLib.toString) ++ extras
+    )
+    .withCompileOptions(conf.compileOptions ++ List(s"-I$include") ++ extras)
+}
--- src/main/scala/test.scala
+++ src/main/scala/test.scala
@@ -1,9 +1,60 @@
-import lib_check.functions.*
-import lib_check.types.*
-import scala.scalanative.unsafe.Zone
+import libraylib.functions.*
+import libraylib.types.*
 
-@main def hello =
+import scala.scalanative.unsafe.*
+import scala.scalanative.unsigned.*
+
+@main def hello_raylib =
+  val screenWidth = 800
+  val screenHeight = 450
+  import libraylib.functions.*
+  import libraylib.types.*
+
+  InitWindow(
+    screenWidth,
+    screenHeight,
+    c"hello raylib from Scala Native"
+  )
+
   Zone { implicit z =>
-    val hello = Hello.apply(25, 135.0)
-    println(s"Result is: ${resolve(!hello, 42)}");
+
+    val WHITE = Color(255.toUByte, 255.toUByte, 255.toUByte, 255.toUByte)
+    val GREEN = Color(0.toUByte, 255.toUByte, 0.toUByte, 255.toUByte)
+    val BLUE = Color(0.toUByte, 0.toUByte, 255.toUByte, 255.toUByte)
+    val BLACK = Color(0.toUByte, 0.toUByte, 0.toUByte, 255.toUByte)
+
+    val redBallPosition = Vector2(screenWidth / 2, screenHeight / 2)
+    val blueBallPositionPtr = Vector2(0, 0)
+    val blueBallPosition = !blueBallPositionPtr
+
+    SetTargetFPS(200)
+
+    var i = 0
+
+    while !WindowShouldClose() do
+
+      !redBallPosition = GetMousePosition()
+      BeginDrawing()
+
+      import KeyboardKey.*
+
+      if IsKeyDown(KEY_RIGHT.int) then blueBallPosition.x += 2.0f;
+      if IsKeyDown(KEY_LEFT.int) then blueBallPosition.x -= 2.0f;
+      if IsKeyDown(KEY_UP.int) then blueBallPosition.y -= 2.0f;
+      if IsKeyDown(KEY_DOWN.int) then blueBallPosition.y += 2.0f;
+
+      ClearBackground(!WHITE)
+      DrawText(
+        c"Red ball will follow your pointer,\n move blue with arrow keys",
+        10,
+        10,
+        20,
+        !BLACK
+      )
+      DrawCircleV(!redBallPosition, 50, !GREEN)
+      DrawCircleV(blueBallPosition, 20, !BLUE)
+      EndDrawing()
+    end while
+    CloseWindow()
   }
+end hello_raylib
